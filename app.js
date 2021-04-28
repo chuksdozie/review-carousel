4c4551ada7e01e18ab71f0b5a038d9136a6b8151
Chuks dozie <chuksdozie48@gmail.com>
2021-04-28 10:27:12 +0100

    adding up the revies next

---
+++ app.js
@@ -0,0 +1,37 @@
+// list of reviews
+reviews = [
+    {
+        revname:"Cally Calista",
+        revjobtitle:"Laundry Expert",
+        revtext:"BLAQ has been the plug to my graphics, I recommend this company for all graphics works, tested and trusted.",
+        revimage:""
+    },
+
+    {
+        revname:"",
+        revjobtitle:"",
+        revtext:"",
+        revimage:""
+    },
+
+    {
+        revname:"",
+        revjobtitle:"",
+        revtext:"",
+        revimage:""
+    },
+
+    {
+        revname:"",
+        revjobtitle:"",
+        revtext:"",
+        revimage:""
+    },
+
+    {
+        revname:"",
+        revjobtitle:"",
+        revtext:"",
+        revimage:""
+    }
+]
