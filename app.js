29de4e8d4ade0708e75dac587123f7b1f6751494
Chuks dozie <chuksdozie48@gmail.com>
2021-04-28 11:25:58 +0100

    file upload

--- app.js
+++ app.js
@@ -4,34 +4,34 @@ reviews = [
         revname:"Cally Calista",
         revjobtitle:"Laundry Expert",
         revtext:"BLAQ has been the plug to my graphics, I recommend this company for all graphics works, tested and trusted.",
-        revimage:""
+        revimage:"cally1.jpg"
     },
 
     {
-        revname:"",
-        revjobtitle:"",
-        revtext:"",
-        revimage:""
+        revname:"Ortiz Jay",
+        revjobtitle:"Rapper/Co-founder, Guyman Republic",
+        revtext:"BLAQ keeps it simple and interesting with enough details that brings out the beauty of his designs",
+        revimage:"cj2.jpg"
     },
 
     {
-        revname:"",
-        revjobtitle:"",
-        revtext:"",
-        revimage:""
+        revname:"Floxy Onyekwem",
+        revjobtitle:"Chief Secretary, Marshall Services",
+        revtext:"Have been seeing so many designs but your is unique. I must confess, the boy is good. Kudos!",
+        revimage:"florence.jpg"
     },
 
     {
-        revname:"",
-        revjobtitle:"",
-        revtext:"",
-        revimage:""
+        revname:"Will Smith",
+        revjobtitle:"Hollywood Actor",
+        revtext:"Honestly, Ama come clean, I dont know sh*t about this guy, he has just been all around, Will can you give me review? Mehn! I just had to give him that review if I wanted some peace.",
+        revimage:"smith.jpg"
     },
 
     {
-        revname:"",
-        revjobtitle:"",
-        revtext:"",
-        revimage:""
+        revname:"Donald Trump",
+        revjobtitle:"Former President Of the United States",
+        revtext:"I wish to skip this review, I mean I dont know this guy, he said its for a project, I guess I will review the option of giving him a review",
+        revimage:"trump.jpg"
     }
 ]
